8a34628288336bb5bf4388f43c1ac19c41dffaa2
philippj <philipp98.joos@gmail.com>
2022-08-27 14:18:24 +0200

    Class structure for dynamic actions

--- Scripts/3_Game/classes.c
+++ Scripts/3_Game/classes.c
@@ -54,3 +54,112 @@ class _Event {
         return this._reference.GetPosition() == other._reference.GetPosition();
     }
 };
+
+class GameLabsActionContext {
+    private string contextType = "world";
+    private Object referencedObject;
+
+    ref map<string, ref GameLabsActionParameter> parameters = new map<string, ref GameLabsActionParameter>;
+
+    void GameLabsActionContext(string contextType, Object referencedObject) {
+        this.contextType = contextType;
+        this.referencedObject = referencedObject;
+    }
+
+    string GetContextType() {
+        return this.contextType;
+    }
+
+    Object GetReferencedObject() {
+        return this.referencedObject;
+    }
+};
+
+class GameLabsActionParameter {
+    string displayName;
+    string description;
+
+    /*
+     * dataType controls what fields are being displayed in the CFCloud interface
+     * Available types:
+     * int: any numeric value
+     * string: any text value
+     * boolean: true or false
+     * cf_itemlist: A dropdown list of all available game items
+     */
+    string dataType;
+
+    int valueInt;
+    string valueString;
+    bool valueBoolean;
+
+    void GameLabsActionParameter(string displayName, string description, string dataType) {
+        this.displayName = displayName;
+        this.description = description;
+        this.dataType = dataType;
+    }
+
+    Object GetValue() {
+        switch(this.dataType) {
+            case "int": {
+                return this.valueInt;
+            }
+            case "string": {
+                return this.valueString;
+            }
+            case "boolean": {
+                return this.valueBoolean;
+            }
+            case "cf_itemlist": {
+                return this.valueString;
+            }
+            default: {
+                return NULL;
+            }
+        }
+    }
+};
+
+class GameLabsContextAction {
+    // A unique actionCode as string, identifying your action in the internal GameLabs system
+    // Convey to a standard "{YourModPrefix}_{Action}" eg. "TestMod_SomeAction"
+    string actionCode;
+
+    // User-friendly name for your action which is being displayed in the CFCloud interface for users
+    string actionName;
+
+    // Prefix your action with an icon in the CFCloud interface, leave empty to disable
+    // Available icons: https://fontawesome.com/v5/search (type is always dualtone)
+    string actionIcon;
+
+    // Colour code for your action, only override if the action you implement is destructive
+    // Available codes: "success" (green), "danger" (red), "warning" (yellow)
+    string actionColour = "default";
+
+    // world, player, vehicle
+    string actionContext = "world";
+
+    ref map<string, ref GameLabsActionParameter> parameters = new map<string, ref GameLabsActionParameter>;
+
+    void GameLabsContextAction() {
+
+    }
+
+    void Register() {
+        GetGameLabs().AddGameLabsAction(this);
+    }
+
+    /*
+     * bool Execute()
+     * Implement any action logic in here which gets triggered based on the API orders module
+     */
+    static bool Execute(GameLabsActionContext context) {
+        return false;
+    }
+
+    string GetActionCode() {return this.actionCode;}
+    string GetActionName() {return this.actionName;}
+    string GetActionIcon() {return this.actionIcon;}
+    string GetActionColour() {return this.actionColour;}
+    string ToJson() { return JsonFileLoader<GameLabsContextAction>.JsonMakeData(this); }
+};
