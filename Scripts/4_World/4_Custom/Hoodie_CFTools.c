841f7aa7d3d429c7211429a2cad4cabf258ea959
philippj <philipp98.joos@gmail.com>
2022-11-05 23:18:36 +0100

    Make assets usable outside of testing environment

--- Scripts/4_World/4_Custom/Hoodie_CFTools.c
+++ Scripts/4_World/4_Custom/Hoodie_CFTools.c
@@ -1,5 +1,15 @@
 class Hoodie_CFTools extends Hoodie_ColorBase {
+
+    void Hoodie_CFTools() {
+        string tmp;
+        if(GetGameLabs().GetConfiguration().GetDebugStatus() && GetGame().CommandlineGetParam("gamelabstesting", tmp)) {
+            this._isTestingEnvironment = true;
+        }
+    }
+
     override bool CanPutInCargo(EntityAI parent) {
+        if(!this._isTestingEnvironment) return super.CanPutInCargo(parent);
+
         PlayerBase player;
         if(parent.GetHierarchyRootPlayer() && PlayerBase.CastTo(player, parent.GetHierarchyRootPlayer())) {
             if(!player.HasAnyIdentitySet()) return true;
@@ -13,6 +23,8 @@ class Hoodie_CFTools extends Hoodie_ColorBase {
     }
 
     override bool CanPutIntoHands(EntityAI parent) {
+        if(!this._isTestingEnvironment) return super.CanPutIntoHands(parent);
+
         PlayerBase player;
         if(parent && PlayerBase.CastTo(player, parent)) {
             if(!player.HasAnyIdentitySet()) return true;
@@ -26,6 +38,8 @@ class Hoodie_CFTools extends Hoodie_ColorBase {
     }
 
     override bool CanPutAsAttachment(EntityAI parent) {
+        if(!this._isTestingEnvironment) return super.CanPutAsAttachment(parent);
+
         PlayerBase player;
         if(parent && PlayerBase.CastTo(player, parent)) {
             if(!player.HasAnyIdentitySet()) return true;
