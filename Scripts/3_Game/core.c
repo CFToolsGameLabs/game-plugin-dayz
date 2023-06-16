129825b62ca39a9bf481a9ed01b142caef44d5ee
philippj <philipp98.joos@gmail.com>
2023-06-17 00:38:57 +0200

    feature: version bump

--- Scripts/3_Game/core.c
+++ Scripts/3_Game/core.c
@@ -1,5 +1,5 @@
 class GameLabsCore {
-    private const string modControlledVersionIdentifier = "1.901";
+    private const string modControlledVersionIdentifier = "1.902";
 
     private ref GameLabsAPI api;
     private ref GameLabsLogger logger;
