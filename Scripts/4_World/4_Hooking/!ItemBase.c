96456232b0e8e5e9869e6045a90be9a9b9a70edc
philippj <philipp98.joos@gmail.com>
2023-11-27 01:18:37 +0100

    feature: track server entity count

---
+++ Scripts/4_World/4_Hooking/!ItemBase.c
@@ -0,0 +1,53 @@
+modded class ItemBase {
+    void ItemBase() {
+        if(!GetGameLabs()) return;
+        if(!GetGameLabs().IsServer()) return;
+        GetGameLabs().IncrEntityCount();
+    }
+
+    void ~ItemBase() {
+        if(!GetGameLabs()) return;
+        if(!GetGameLabs().IsServer()) return;
+        GetGameLabs().DecrEntityCount();
+    }
+};
+
+
+modded class ZombieBase  {
+    private ref _AI _registeredInstance = new _AI(this, true);
+
+    void ZombieBase () {
+        if(!GetGameLabs()) return;
+        if(!GetGameLabs().IsServer()) return;
+        GetGameLabs().IncrAICount();
+        GetGameLabs().RegisterAI(this._registeredInstance);
+    }
+
+    void ~ZombieBase () {
+        if(!GetGameLabs()) return;
+        if(!GetGameLabs().IsServer()) return;
+        GetGameLabs().DecrAICount();
+        if(this._registeredInstance) GetGameLabs().RemoveAI(this._registeredInstance);
+    }
+
+    override void EEHitBy(TotalDamageResult damageResult, int damageType, EntityAI source, int component, string dmgZone, string ammo, vector modelPos, float speedCoef) {
+        super.EEHitBy(damageResult, damageType, source, component, dmgZone, ammo, modelPos, speedCoef);
+
+        if(damageType != DamageType.FIRE_ARM) return;
+
+        if( !IsAlive() ) return;
+
+        PlayerBase player;
+        if(source) {
+            player = PlayerBase.Cast(source.GetHierarchyRootPlayer());
+        }
+        if(!player) return;
+
+        if(player.HasUpstreamIdentity()) {
+            string cftoolsId = player.GetUpstreamIdentity();
+            ref GLPlayerStatistics playerStatistics = GetGameLabs().GetPlayerStatisticsByCFToolsId(cftoolsId);
+            playerStatistics.shotsHit++;
+            playerStatistics.shotsHitInfected++;
+        }
+    }
+};
