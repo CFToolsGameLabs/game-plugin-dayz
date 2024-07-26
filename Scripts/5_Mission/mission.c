4afce9b9533cd7717ed7708a1b34bb416fc8e8d1
philippj <philipp98.joos@gmail.com>
2024-07-26 10:57:25 +0200

    chore: disabled old expansion mod sync

--- Scripts/5_Mission/mission.c
+++ Scripts/5_Mission/mission.c
@@ -363,6 +363,8 @@ modded class MissionGameplay extends MissionBase {
         if(eventTypeId == ChatMessageEventTypeID) {
             chatParams = ChatMessageEventParams.Cast(params);
 
+            // Old expansion stuff is deprecated
+            /*
             #ifdef EXPANSIONMODCORE
             if(chatParams.param1 > CCBattlEye) {
                 if(this.name == chatParams.param2) {
@@ -375,7 +377,8 @@ modded class MissionGameplay extends MissionBase {
                     this.gameLabsClient.SyncExpansionChat(chatParams);
                 }
             }
-            #endif                
+            #endif
+            */
 
             if(m_LifeState == EPlayerStates.ALIVE) {
                 int channel = chatParams.param1;
