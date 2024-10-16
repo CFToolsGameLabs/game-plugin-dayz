d6ee0705eb24cda5bcdfdb49d5665e4e99b5a5ae
philippj <philipp98.joos@gmail.com>
2024-10-16 12:26:19 +0200

    chore: version increment

--- Scripts/3_Game/core.c
+++ Scripts/3_Game/core.c
@@ -1,5 +1,5 @@
 class GameLabsCore {
-    private const string modControlledVersionIdentifier = "1.935";
+    private const string modControlledVersionIdentifier = "1.936";
 
     private ref GameLabsAPI api;
     private ref GameLabsLogger logger;
