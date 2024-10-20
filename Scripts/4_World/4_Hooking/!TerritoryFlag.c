cf29c99275e35f28c277dfd32aa093ffc347e2d7
philippj <philipp98.joos@gmail.com>
2024-10-20 12:02:05 +0200

    feature: auto update lifetime for event display name

--- Scripts/4_World/4_Hooking/!TerritoryFlag.c
+++ Scripts/4_World/4_Hooking/!TerritoryFlag.c
@@ -1,15 +1,22 @@
 modded class TerritoryFlag extends BaseBuildingBase {
     ref _Event _registeredInstance;
+    private ref Timer timerUpdateGL;
 
     void TerritoryFlag () {
         // This defers initialization of the GameLabs _Event as it probably breaks execution of other modded
         // constructors because it uses a hard ref to the instance
-        GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).Call(this._InitGameLabs);
+        if(GetGameLabs()) {
+            if(GetGameLabs().IsServer()) {
+                GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).Call(this._InitGameLabs);
+                this.timerUpdateGL = new Timer(CALL_CATEGORY_SYSTEM);
+                // 3601 to ensure the timer doesnt accidentally catch the same hour due to rounding
+                this.timerUpdateGL.Run(3601, this, "_updateEvent", NULL, true);
+            }
+        }
 	}
 
     private void _InitGameLabs() {
-        float remainingLifetime = GetLifetime() / 3600;
-        this._registeredInstance = new _Event(this.GetType(), "pennant", this, "<b>Territory Flag</b><br/>Flag Level: " + Math.Round(GetRefresherTime01() * 100) + " %<br/>Remaining Lifetime: ~ " + Math.Round(remainingLifetime) + " hours");
+        this._registeredInstance = new _Event(this.GetType(), "pennant", this, GL_GetDisplayName());
 
         if(GetGameLabs()) {
             if(GetGameLabs().IsServer()) {
@@ -22,8 +29,28 @@ modded class TerritoryFlag extends BaseBuildingBase {
         if(GetGameLabs()) {
             if(GetGameLabs().IsServer()) {
                 if(this._registeredInstance) GetGameLabs().RemoveEvent(this._registeredInstance);
+                if(this.timerUpdateGL) this.timerUpdateGL.Stop();
             }
         }
+    }
+
+    private string GL_GetDisplayName() {
+        float remainingLifetime = GetLifetime() / 3600;
+        string displayName = string.Format("<b>Territory Flag</b><br/>Flag Level: %1 %%<br/>Remaining Lifetime: ~ %2 hours", Math.Round(GetRefresherTime01() * 100), Math.Round(remainingLifetime));
+        return displayName;
+    }
+
+    // Called each hour to update remaining lifetime
+    private void _updateEvent() {
+        _Event eventEntity = _GetEventInstance();
+        if(eventEntity) {
+            string displayName = GL_GetDisplayName();
+            if(eventEntity.DisplayName() != displayName) {
+                GetGameLabs().RemoveEvent(this._registeredInstance);
+                this._InitGameLabs();
+            }
+        }
+        // The else case should never happen, this is just an integrity check
     }
 
     // Public API
