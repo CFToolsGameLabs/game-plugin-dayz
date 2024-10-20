95e6254ee41931dc2811508c71aad9e96e826854
philippj <philipp98.joos@gmail.com>
2024-10-20 19:37:36 +0200

    feature: owner tracking for base building components

--- Scripts/4_World/4_Hooking/!FenceKit.c
+++ Scripts/4_World/4_Hooking/!FenceKit.c
@@ -1,4 +1,4 @@
-modded class TerritoryFlagKit extends KitBase {
+modded class FenceKit extends KitBase {
     override void OnPlacementComplete( Man player, vector position = "0 0 0", vector orientation = "0 0 0" ) {
         super.OnPlacementComplete(player, position, orientation);
 
@@ -12,24 +12,24 @@ modded class TerritoryFlagKit extends KitBase {
 
             GetGame().GetObjectsAtPosition(playerPos, 1.0, nearestObjects, null);
 
-            TerritoryFlag totem;
+            Fence relatedObject;
             foreach (Object nearestObject : nearestObjects) {
                 EntityAI ent = EntityAI.Cast(nearestObject);
                 if(ent) {
-                    if(ent.GetType() == "TerritoryFlag") {
-                        totem = TerritoryFlag.Cast(ent);
+                    if(ent.GetType() == "Fence") {
+                        relatedObject = Fence.Cast(ent);
                         break;
                     }
                 }
             }
 
-            if(totem != NULL) {
-                GetGameLabs().GetLogger().Debug(string.Format("TerritoryFlagKit.OnPlacementComplete > totem=%1; playerBase=%2; steam64=%3", totem, playerBase, steam64));
-                totem.GL_SetSteam64(steam64);
-                totem.GL_GatherPersistentID();
-                totem.GL_Save(true);
+            if(relatedObject != NULL) {
+                GetGameLabs().GetLogger().Debug(string.Format("FenceKit.OnPlacementComplete > relatedObject=%1; playerBase=%2; steam64=%3", relatedObject, playerBase, steam64));
+                relatedObject.GL_SetSteam64(steam64);
+                relatedObject.GL_GatherPersistentID();
+                relatedObject.GL_Save(true);
             } else {
-                GetGameLabs().GetLogger().Debug(string.Format("TerritoryFlagKit.OnPlacementComplete > TOTEM_NOT_FOUND; playerBase=%1; steam64=%2", playerBase, steam64));
+                GetGameLabs().GetLogger().Debug(string.Format("FenceKit.OnPlacementComplete > RELATED_OBJECT_NOT_FOUND; playerBase=%1; steam64=%2", playerBase, steam64));
             }
         }
     }
