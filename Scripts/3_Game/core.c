79b55dcd9e897073b9f62afd975d45a1f4547ee6
philippj <philipp98.joos@gmail.com>
2025-03-09 18:04:48 +0100

    chore: bump version

--- Scripts/3_Game/core.c
+++ Scripts/3_Game/core.c
@@ -1,5 +1,5 @@
 class GameLabsCore {
-    private const string modControlledVersionIdentifier = "1.941";
+    private const string modControlledVersionIdentifier = "1.953";
 
     private ref GameLabsAPI api;
     private ref GameLabsLogger logger;
