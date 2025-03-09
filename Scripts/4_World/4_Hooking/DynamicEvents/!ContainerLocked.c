b3912b08b98e339d5945df5a1270e39d4b375fe0
philippj <philipp98.joos@gmail.com>
2025-03-09 18:04:27 +0100

    feature: tracking for locked containers and scientific briefcase

--- Scripts/4_World/4_Hooking/DynamicEvents/!ContainerLocked.c
+++ Scripts/4_World/4_Hooking/DynamicEvents/!ContainerLocked.c
@@ -1,71 +1,71 @@
 modded class Land_ContainerLocked_Blue_DE : ContainerLockedBase {
-    ref _Event _registeredInstance = new _Event(this.GetType(), "container-storage", this, "Locked Container");
+    ref _Event gl_registeredInstance = new _Event(this.GetType(), "container-storage", this, "Locked Container");
 
     override void EEInit() {
         super.EEInit();
         if(!GetGame().IsServer()) return;
         vector position = GetPosition();
         if(position[0] <= 0 && position[1] <= 0 && position[2] <= 0) return;
-        GetGameLabs().RegisterEvent(this._registeredInstance);
+        GetGameLabs().RegisterEvent(this.gl_registeredInstance);
     }
     override void EEDelete(EntityAI parent) {
         super.EEDelete(parent);
         if(!GetGame().IsServer()) return;
         if(!GetGameLabs()) return;
-        if(this._registeredInstance) GetGameLabs().RemoveEvent(this._registeredInstance);
+        if(this.gl_registeredInstance) GetGameLabs().RemoveEvent(this.gl_registeredInstance);
     }
 };
 
 modded class Land_ContainerLocked_Yellow_DE  : ContainerLockedBase {
-    ref _Event _registeredInstance = new _Event(this.GetType(), "container-storage", this, "Locked Container");
+    ref _Event gl_registeredInstance = new _Event(this.GetType(), "container-storage", this, "Locked Container");
 
     override void EEInit() {
         super.EEInit();
         if(!GetGame().IsServer()) return;
         vector position = GetPosition();
         if(position[0] <= 0 && position[1] <= 0 && position[2] <= 0) return;
-        GetGameLabs().RegisterEvent(this._registeredInstance);
+        GetGameLabs().RegisterEvent(this.gl_registeredInstance);
     }
     override void EEDelete(EntityAI parent) {
         super.EEDelete(parent);
         if(!GetGame().IsServer()) return;
         if(!GetGameLabs()) return;
-        if(this._registeredInstance) GetGameLabs().RemoveEvent(this._registeredInstance);
+        if(this.gl_registeredInstance) GetGameLabs().RemoveEvent(this.gl_registeredInstance);
     }
 };
 
 modded class Land_ContainerLocked_Orange_DE  : ContainerLockedBase {
-    ref _Event _registeredInstance = new _Event(this.GetType(), "container-storage", this, "Locked Container");
+    ref _Event gl_registeredInstance = new _Event(this.GetType(), "container-storage", this, "Locked Container");
 
     override void EEInit() {
         super.EEInit();
         if(!GetGame().IsServer()) return;
         vector position = GetPosition();
         if(position[0] <= 0 && position[1] <= 0 && position[2] <= 0) return;
-        GetGameLabs().RegisterEvent(this._registeredInstance);
+        GetGameLabs().RegisterEvent(this.gl_registeredInstance);
     }
     override void EEDelete(EntityAI parent) {
         super.EEDelete(parent);
         if(!GetGame().IsServer()) return;
         if(!GetGameLabs()) return;
-        if(this._registeredInstance) GetGameLabs().RemoveEvent(this._registeredInstance);
+        if(this.gl_registeredInstance) GetGameLabs().RemoveEvent(this.gl_registeredInstance);
     }
 };
 
 modded class Land_ContainerLocked_Red_DE  : ContainerLockedBase {
-    ref _Event _registeredInstance = new _Event(this.GetType(), "container-storage", this, "Locked Container");
+    ref _Event gl_registeredInstance = new _Event(this.GetType(), "container-storage", this, "Locked Container");
 
     override void EEInit() {
         super.EEInit();
         if(!GetGame().IsServer()) return;
         vector position = GetPosition();
         if(position[0] <= 0 && position[1] <= 0 && position[2] <= 0) return;
-        GetGameLabs().RegisterEvent(this._registeredInstance);
+        GetGameLabs().RegisterEvent(this.gl_registeredInstance);
     }
     override void EEDelete(EntityAI parent) {
         super.EEDelete(parent);
         if(!GetGame().IsServer()) return;
         if(!GetGameLabs()) return;
-        if(this._registeredInstance) GetGameLabs().RemoveEvent(this._registeredInstance);
+        if(this.gl_registeredInstance) GetGameLabs().RemoveEvent(this.gl_registeredInstance);
     }
 };
--- Scripts/4_World/4_Hooking/DynamicEvents/!ScientificBriefcase.c
+++ Scripts/4_World/4_Hooking/DynamicEvents/!ScientificBriefcase.c
@@ -1,17 +1,17 @@
 modded class ScientificBriefcase : Container_Base {
-    ref _Event _registeredInstance = new _Event(this.GetType(), "briefcase", this, "Scientific Briefcase");
+    ref _Event gl_registeredInstance = new _Event(this.GetType(), "briefcase", this, "Scientific Briefcase");
 
     override void EEInit() {
         super.EEInit();
         if(!GetGame().IsServer()) return;
         vector position = GetPosition();
         if(position[0] <= 0 && position[1] <= 0 && position[2] <= 0) return;
-        GetGameLabs().RegisterEvent(this._registeredInstance);
+        GetGameLabs().RegisterEvent(this.gl_registeredInstance);
     }
     override void EEDelete(EntityAI parent) {
         super.EEDelete(parent);
         if(!GetGame().IsServer()) return;
         if(!GetGameLabs()) return;
-        if(this._registeredInstance) GetGameLabs().RemoveEvent(this._registeredInstance);
+        if(this.gl_registeredInstance) GetGameLabs().RemoveEvent(this.gl_registeredInstance);
     }
 };
