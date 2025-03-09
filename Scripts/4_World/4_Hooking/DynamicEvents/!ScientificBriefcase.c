2537fd6f03f0e087b5728c2d860381f0294d17fa
philippj <philipp98.joos@gmail.com>
2025-03-09 16:20:39 +0100

    feature: track scientific briefcase

---
+++ Scripts/4_World/4_Hooking/DynamicEvents/!ScientificBriefcase.c
@@ -0,0 +1,17 @@
+modded class ScientificBriefcase : Container_Base {
+    ref _Event _registeredInstance = new _Event(this.GetType(), "briefcase", this, "Scientific Briefcase");
+
+    override void EEInit() {
+        super.EEInit();
+        if(!GetGame().IsServer()) return;
+        vector position = GetPosition();
+        if(position[0] <= 0 && position[1] <= 0 && position[2] <= 0) return;
+        GetGameLabs().RegisterEvent(this._registeredInstance);
+    }
+    override void EEDelete(EntityAI parent) {
+        super.EEDelete(parent);
+        if(!GetGame().IsServer()) return;
+        if(!GetGameLabs()) return;
+        if(this._registeredInstance) GetGameLabs().RemoveEvent(this._registeredInstance);
+    }
+};
